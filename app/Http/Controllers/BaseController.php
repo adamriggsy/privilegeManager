17a26ea1893df2c5d866a7f3c73ff3e1b0bc5dee
Adam Riggs <ajriggs@afstores.com>
2019-03-11 15:48:13 -0600

    auto authorization for testing purposes

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -10,6 +10,10 @@ class BaseController extends Controller
 
     public function __construct(Request $request)
     {
+        if(!\Auth::check()){
+        	\Auth::attempt(['email' => 'adam.riggsy@gmail.com', 'password' => 'Sophie2006*']);
+        }
+
         if(!is_null($request->route()->getPrefix()) && $request->route()->getPrefix() == "/json"){
         	$this->jsonRequest = true;
         }
