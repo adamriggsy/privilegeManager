55442083e3b351ce1af9c3c8841d7a22b5e8507a
Adam Riggs <ajriggs@afstores.com>
2019-03-15 23:08:26 -0600

    new verbiage submit page app

--- resources/views/includes/child/jsonFormSubmission.blade.php
+++ resources/views/includes/child/jsonFormSubmission.blade.php
@@ -3,6 +3,7 @@
 		
 	</head>
 	<body>
-		<p>The process is finished.</p>
+		<p style="margin-top:20px; text-align: center;">The process is finished.</p>
+		<p style="text-align: center;">Make sure to refresh the status page to see the updated privileges</p>
 	</body>
 </html>
